b53238b4a81fabe5b2ccaf7554b6a8e6f3e4de60
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-29 13:42:19 -0400

    added some variables and a streamreader. added path to csv.

---
+++ morsecodeconverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morsecodeconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var morseCodePath = "morse.csv";
+            var userInput = Convert.ToChar(Console.ReadLine());
+            using (var reader = new StreamReader(morseCodePath))
+            {
+
+            }
+
+            Console.WriteLine("Enter a string of words and I will morsify it: ");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
