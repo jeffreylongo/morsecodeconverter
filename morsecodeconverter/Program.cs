25c8adcbb0490a339faf11286850871db495d9db
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-29 14:08:34 -0400

    added a dictionary and parsing the csv file into it.

--- morsecodeconverter/Program.cs
+++ morsecodeconverter/Program.cs
@@ -13,9 +13,16 @@ namespace morsecodeconverter
         {
             var morseCodePath = "morse.csv";
             var userInput = Convert.ToChar(Console.ReadLine());
+            Dictionary<char, string> morseCodeDictionary = new Dictionary<char, string>();
+
             using (var reader = new StreamReader(morseCodePath))
             {
-
+                
+                while (reader.Peek() >= 0)
+                {
+                   var text = reader.ReadLine().Split(',');
+                    morseCodeDictionary.Add(char.Parse(text[0]), text[1]);
+                }
             }
 
             Console.WriteLine("Enter a string of words and I will morsify it: ");
